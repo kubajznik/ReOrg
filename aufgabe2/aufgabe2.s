b1144848dcf76a7a94e012af2b11125a1a6836d9
kubajznik <35335130+kubajznik@users.noreply.github.com>
2018-01-11 13:23:47 +0100

    Add files via upload

---
+++ aufgabe2/aufgabe2.s
@@ -0,0 +1,147 @@
+# v180105
+
+#########################################
+# Vorgabe: find_str
+#########################################
+# $a0: haystack
+# $a1: len of haystack
+# $a2: needle
+# $a3: len of needle
+# $v0: relative position of needle, -1 if not found
+
+find_str:
+    # save $ra on stack
+    addi $sp, $sp, -4
+    sw $ra, 0($sp)
+
+    # save beginning of haystack
+    move $t5, $a0
+    # save len of needle
+    move $t4, $a3
+
+    # calc end address of haystick and needle
+    add $a1, $a1, $a0
+    add $a3, $a3, $a2
+
+haystick_loop:
+    bge $a0, $a1, haystick_loop_end
+
+    move $t6, $a0
+    move $t7, $a2
+needle_loop:
+    # load char from haystick
+    lbu $t0, 0($t6)
+    # load char from needle
+    lbu $t1, 0($t7)
+
+    bne $t0, $t1, needle_loop_end
+
+    addi $t6, $t6, 1
+    addi $t7, $t7, 1
+
+    # reached end of needle
+    bge $t7, $a3, found_str
+
+    # reached end of haystick
+    bge $t6, $a1, found_nostr
+
+    j needle_loop
+needle_loop_end:
+
+    addi $a0, $a0, 1
+    j haystick_loop
+haystick_loop_end:
+
+found_nostr:
+    # prepare registers so found_str: produces -1
+    li $t6, 0
+    li $t5, 0
+    li $t4, 1
+
+found_str:
+    sub $v0, $t6, $t5
+    sub $v0, $v0, $t4
+
+
+    # restore $ra from stack
+    lw $ra, 0($sp)
+    addi $sp, $sp, 4
+    jr $ra
+
+#########################################
+# Aufgabe 2: Spamfilter
+#########################################
+# $v0: Spamscore
+
+spamfilter:
+    ### Register gemaess Registerkonventionen sichern
+
+    ### Badwords liegen im Puffer badwords_buffer
+    ### Der Text der E-Mail liegt im Puffer email_buffer
+   
+    ### Schleife ueber Bad words (wort1,gewicht1,wort2,gewicht2,...)
+        ### lese ein Wort
+        ### lese und konvertiere Gewicht
+        ### suche alle Vorkommen des Wortes im Text der E-Mail und addiere Gewicht
+
+    ### Rueckgabewert setzen
+    ### Register wieder herstellen
+    jr $ra
+
+#########################################
+#
+
+#
+# data
+#
+
+.data
+
+email_buffer: .asciiz "Hochverehrte Empfaenger,\n\nbei dieser E-Mail handelt es sich nicht um Spam sondern ich moechte Ihnen\nvielmehr ein lukratives Angebot machen: Mein entfernter Onkel hat mir mehr Geld\nhinterlassen als in meine Geldboerse passt. Ich muss Ihnen also etwas abgeben.\nVorher muss ich nur noch einen Spezialumschlag kaufen. Senden Sie mir noch\nheute BTC 1,000 per Western-Union und ich verspreche hoch und heilig Ihnen\nalsbald den gerechten Teil des Vermoegens zu vermachen.\n\nHochachtungsvoll\nAchim Mueller\nSekretaer fuer Vermoegensangelegenheiten\n"
+
+size: .word 538
+
+badwords_buffer: .asciiz "Spam,5,Geld,1,ROrg,0,lukrativ,3,Kohlrabi,10,Weihnachten,3,Onkel,7,Vermoegen,2,Brief,4,Lotto,3"
+badwords_size: .word 93
+
+spamscore_text: .asciiz "Der Spamscore betraegt: "
+
+#
+# main
+#
+
+.text
+.globl main
+
+main:
+    # Register sichern
+    addi $sp, $sp, -8
+    sw $ra, 0($sp)
+    sw $s0, 4($sp)
+
+
+    jal spamfilter
+    move $s0, $v0
+
+
+    li $v0, 4
+    la $a0, spamscore_text
+    syscall
+    move $a0, $s0
+    li $v0, 1
+    syscall
+
+    li $v0, 11
+    li $a0, 10
+    syscall
+
+
+    # Register wieder herstellen
+    lw $ra, 0($sp)
+    lw $s0, 4($sp)
+    addi $sp, $sp, 8
+    jr $ra
+
+#
+# end main
+#
---
+++ aufgabe3/aufgabe3.s
@@ -0,0 +1,169 @@
+# v171215
+
+#########################################
+# Vorgabe: read_email
+#########################################
+# $a0: buffer
+# $v0: number of characters read
+
+read_email:
+    move $t0, $a0
+
+    # read mail from disk
+    li $v0, 13
+    la $a0, input_file
+    li $a1, 0
+    li $a2, 0
+    syscall
+
+    # save fd
+    move $t1, $v0
+
+    # read to buffer
+    li $v0, 14
+    move $a0, $t1
+    move $a1, $t0 # address of buffer
+    li $a2, 4096
+    syscall
+
+    move $t0, $v0
+
+    # close file
+    li $v0, 16
+    move $a0, $t1 # fd
+    syscall
+
+    move $v0, $t0
+
+    jr $ra
+
+#########################################
+# Vorgabe: write_email
+#########################################
+# $a0: buffer
+# $a1: number of characters to write
+# $a2: truncate file
+
+write_email:
+    addi $sp, $sp, -16
+    sw $ra, 0($sp)
+    sw $s0, 4($sp)
+    sw $s1, 8($sp)
+    sw $s2, 12($sp)
+
+    move $s0, $a0
+    move $s1, $a1
+
+    # open file
+    li $v0, 13
+    la $a0, output_file
+
+    bne $zero, $a2, write_email_trunc
+    j write_email_notrunc
+    write_email_trunc:
+    li $a1, 0x241       # mode O_WRONLY | O_CREAT | O_TRUNC
+    j write_email_else
+
+    write_email_notrunc:
+    li $a1, 0x441       # mode O_WRONLY | O_CREAT | O_APPEND
+
+    write_email_else:
+    li $a2, 0x1a4
+    syscall             # fd in $v0
+
+    move $s2, $v0       # save fd
+
+    li $v0, 15          # write to file
+    move $a0, $s2
+    move $a1, $s0
+    move $a2, $s1
+    syscall
+
+    # close file
+    li $v0, 16
+    move $a0, $s2
+    syscall
+
+    lw $ra, 0($sp)
+    lw $s0, 4($sp)
+    lw $s1, 8($sp)
+    lw $s2, 12($sp)
+    addi $sp, $sp, 16
+    jr $ra
+
+
+#########################################
+# Aufgabe 3: Ausgabe
+#########################################
+# a0: buffer
+# a1: buffer length
+# a2: relative position of subject
+# a3: spam flag
+
+print_email:
+    ### Register gemass Konventionen sichern
+
+    ### hier implementieren
+
+    ### gesicherte Register wieder herstellen
+    jr $ra
+
+
+#########################################
+#
+
+#
+# data
+#
+
+.data
+
+input_file: .asciiz "../email1"
+output_file: .asciiz "output"
+email_buffer: .space 4096
+
+spam_flag: .asciiz "[SPAM] "
+spam_flag_length: .word 7
+
+#
+# main
+#
+
+.text
+.globl main
+
+main:
+    # Register sichern
+    addi $sp, $sp, -12
+    sw $ra, 0($sp)
+    sw $s0, 4($sp)
+    sw $s1, 8($sp)
+
+    # E-Mail einlesen
+    la $a0, email_buffer
+    jal read_email
+
+    la $a0, email_buffer
+
+    # Groesse
+    move $a1, $v0
+
+    # Position des Subjekts
+    li $a2, 292
+
+    # Spam
+    li $a3, 1
+
+    # E-Mail ausgeben
+    jal print_email
+
+    # Register wieder herstellen
+    lw $ra, 0($sp)
+    lw $s0, 4($sp)
+    lw $s1, 8($sp)
+    addi $sp, $sp, 12
+    jr $ra
+
+#
+# end main
+#
