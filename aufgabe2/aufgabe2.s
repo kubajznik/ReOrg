a2477f9c7c5532e6057660e61a8704905c762410
kubajznik <35335130+kubajznik@users.noreply.github.com>
2018-01-18 01:13:09 +0100

    Kaffee

--- aufgabe2/aufgabe2.s
+++ aufgabe2/aufgabe2.s
@@ -190,7 +190,7 @@ spamfilter:
 		move $a1, $s7			# Letzte Listenlaenge laden
 		sub $a1, $a1, $s1		# Davon die Wortlaenge abziehen
 		sub $a1, $a1, $t8		# Und nochmal 3 abziehen; Komma,Zahl,Komma
-		addi $a1, 2		
+	
 		move $a0, $a1
 		li $v0, 1
 		syscall
@@ -231,7 +231,7 @@ email_buffer: .asciiz "Hochverehrte Empfaenger,\n\nbei dieser E-Mail handelt es 
 
 size: .word 538
 
-badwords_buffer: .asciiz "Spam,5,Geld,1,ROrg,10,lukrativ,3,Kohlrabi,1,Weihnachten,3,Onkel,7,Vermoegen,2,Brief,4,Lotto,3"
+badwords_buffer: .asciiz "Spam,5,Geld,1,ROrg,10,lukrativ,3,Kohlrabi,1,Weihnachten,3,Onkel,10,Vermoegen,2,Brief,4,Lotto,3"
 badwords_size: .word 92
 
 badwords_sep: .asciiz ","
